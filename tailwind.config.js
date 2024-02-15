7cafd61fb0a5bfedc4274a6b4b1eaf31ec99dc3f
luiz-matheus-da-silva <134899797+luiz-matheus-da-silva@users.noreply.github.com>
2024-02-15 11:19:08 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.html',
+  './src/**/*.js',],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3A3A3A",
+        secundary: "#939393",
+        third: '#503F2B',
+        bgfooter: '#D9D9D9',
+      }
+    },
+  },
+  plugins: [],
+}
